f496092e2e2369de007c99c6b6a5bfd8bbb7f2f1
elidubon <84058714+elidubon@users.noreply.github.com>
2025-02-23 20:51:45 -0600

    se agregan validaciones como tambien se crean servicios

--- back/sistema-multas/src/main/java/muni/sistema_multas/controlador/IngreMultaController.java
+++ back/sistema-multas/src/main/java/muni/sistema_multas/controlador/IngreMultaController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/multas")
+@CrossOrigin(origins = "http://localhost:4200") // Permite solicitudes desde Angular
 public class IngreMultaController {
     @Autowired
     private IngreMultas multaService;
